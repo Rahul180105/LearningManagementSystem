f8512b64d7641be1d95e1c35c1ed21f77d6f9ad7
Rahul <rahul.s@mindfiresolutions.com>
2026-02-18 22:48:25 +0530

    fix:tailwind integration

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,1 +1,24 @@
 import { Route,Routes } from "react-router-dom";
+import PublicLayout from "./layouts/public.layout";
+import MainLayout from "./layouts/main.layout";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+
+function App(){
+  return(
+    <>
+    <Routes>
+      {/* Public */}
+      <Route element={<PublicLayout/>}>
+      <Route path='/' element={<Login/>}/>
+      </Route>
+      {/* Protected */}
+      <Route element={<MainLayout/>}>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      </Route>
+      {/* Protected */}
+    </Routes>
+    </>
+  )
+}
+export default App;
